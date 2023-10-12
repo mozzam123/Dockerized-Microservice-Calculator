8f97ed15dddaf6c2c3641bcc10a2da5ca3822bd4
mozzam123 <mozzam607@gmail.com>
2023-10-12 16:42:37 +0530

    seperate index and app code

---
+++ calculator-service/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path")
+
+
+// Create Express app instance
+const app = express();
+
+// Define template path
+const template_path = path.join(__dirname, "views");
+
+// Import routes
+const calcRoutes = require("./routes/calculator");
+
+// Serve static files from public directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Parse request bodies
+app.use(express.urlencoded({ extended: false }));
+
+// Set view engine and template path
+app.set("view engine", "hbs");
+app.set("views", template_path);
+app.set("views", path.join(__dirname, "views"));
+
+// Register routes
+app.use(calcRoutes);
+
+
+module.exports = app
