f66f33e08098b4f7495d3b61c140833a8fe4b614
mozzam123 <mozzam607@gmail.com>
2023-11-23 22:55:26 +0530

    changed folder structure by adding routes folder

--- investment-service/app.js
+++ investment-service/app.js
@@ -7,16 +7,18 @@ const path = require("path");
 // Define template path
 const template_path = path.join(__dirname, "views");
 
+// Import routes
+const mutualFundRoutes = require("./routes/mutualFunds");
+
 // Serve static files from public directory
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 
 // Set view engine and template path
 app.set("view engine", "hbs");
 app.set("views", template_path);
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+// Register Routes
+app.use(mutualFundRoutes);
 
 app.listen(INV_PORT, () => {
   console.log(`Server running on http://127.0.0.1:${INV_PORT}`);
